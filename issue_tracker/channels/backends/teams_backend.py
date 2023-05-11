96a9675bd1a80bbc2ec31e467aaf189352b69d9b
rumitpys <rumit@pysquad.com>
2023-05-11 15:23:58 +0530

    [feat]:Integrate Discord channel notifications for issue tracking.

---
+++ issue_tracker/channels/backends/teams_backend.py
@@ -0,0 +1,15 @@
+from .discord_backend import IssueTrackerChannel
+
+
+class TeamsChannel(IssueTrackerChannel):
+    """
+    Sends notifications to a Microsoft Teams channel.
+    """
+    def send_notification(self, **kwargs):
+        """Send a notification to Microsoft Teams.
+
+        This method should be implemented to include the logic for sending a notification to a Microsoft Teams channel.
+
+        Args:
+            **kwargs: A dictionary of keyword arguments containing the information required for sending the notification
+        """
