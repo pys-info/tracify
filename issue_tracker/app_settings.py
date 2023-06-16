fd1b323161e14835868ff57258ed3c3486986c91
Umang Patel <umang-pysquad>
2023-06-16 15:51:37 +0530

    [FIX]: Fix templates directory missing issue - Add Exception for Email configuration

--- issue_tracker/app_settings.py
+++ issue_tracker/app_settings.py
@@ -1,5 +1,12 @@
 from django.conf import settings
 
 ISSUE_TRACKER_CHANNELS_CONFIGURATION = settings.ISSUE_TRACKER_CHANNELS_CONFIGURATION
-EMAIL_ADMIN_USER = settings.EMAIL_ADMIN_USER
-EMAIL_HOST_USER = settings.EMAIL_HOST_USER
+
+if settings.ISSUE_TRACKER_CHANNELS_CONFIGURATION.get('EMAIL', False):
+    if settings.EMAIL_ADMIN_USER and settings.EMAIL_HOST_USER:
+        EMAIL_ADMIN_USER = settings.EMAIL_ADMIN_USER
+        EMAIL_HOST_USER = settings.EMAIL_HOST_USER
+    else:
+        raise AttributeError(
+            "EMAIL_ADMIN_USER / EMAIL_HOST_USER is not defined in settings.py"
+        )
